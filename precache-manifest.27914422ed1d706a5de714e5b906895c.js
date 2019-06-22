300b2b462d2f7a653ca0a269ae4b13405188c8bb
Igor Khlystun <hlustyn7@gmail.com>
2019-06-22 15:19:04 +0300

    Updates

---
+++ precache-manifest.27914422ed1d706a5de714e5b906895c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce29bcd2b9ce454cbe613d83b596f5bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95218e8b17de178eb7d1",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "af0d7bc4bc9f501a2d2c",
+    "url": "/static/css/main.288ed631.chunk.css"
+  },
+  {
+    "revision": "95218e8b17de178eb7d1",
+    "url": "/static/js/2.321c096e.chunk.js"
+  },
+  {
+    "revision": "af0d7bc4bc9f501a2d2c",
+    "url": "/static/js/main.3d6d44ff.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
